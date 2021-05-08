a4dece6863a4dfa71259c21c15a0e2a457f4259f
arima <arima.toshiki@gmail.com>
2021-05-08 17:52:49 +0900

    setup eslint and prettier

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: 'electron-renderer',
+  entry: './src/index.tsx',
+  cache: true,
+  mode: 'development', // "production" | "development" | "none"
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    // Webpack plugin を利用する
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+    }),
+  ],
+};
